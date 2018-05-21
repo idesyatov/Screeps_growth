1c3de8ee6f177374b60172b7abbc94a3a7a967f4
idesyatov <regull@bk.ru>
2018-05-21 23:08:55 +0300

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -2,31 +2,27 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy < creep.carryCapacity) {
-            var source = _.sample(creep.room.find(FIND_SOURCES));
-            result = creep.harvest(source)
-            if(result == OK) {
-                return
-            } else if(result == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source);
-            } else {
-                creep.say(result)
+        if(creep.carry.energy < creep.carryCapacity) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
         else {
-            var spawn = Game.spawns.Spawn1
-            result = creep.transfer(spawn, RESOURCE_ENERGY)
-            if(result == OK){
-                return
-            }else if(result == ERR_NOT_IN_RANGE) {
-                creep.moveTo(spawn);
-            }else if(result == ERR_FULL) {
-                creep.drop(RESOURCE_ENERGY);
-            } else {
-                creep.say(result);
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
             }
         }
-	}
+    }
 };
 
 module.exports = roleHarvester;
