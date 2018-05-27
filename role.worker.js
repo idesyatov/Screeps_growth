4a4ac7162719333b42af4519c0a527ca7e75fe7f
idesyatov <regull@bk.ru>
2018-05-27 15:05:35 +0300

    fix

--- role.worker.js
+++ role.worker.js
@@ -2,16 +2,32 @@ var roleWorker = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.carry.energy < creep.carryCapacity) {
-            console.log(creep.room.find(FIND_SOURCES).length)
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[1]);
+        if (creep.memory.upgrading && creep.carry.energy == 0) {
+            creep.memory.upgrading = false;
+            creep.say('harvesting');
+          }
+          if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.upgrading = true;
+            creep.say('upgrading');
+          }
+        
+          if (creep.memory.upgrading) {
+            creep.memory.target = undefined;
+            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(creep.room.controller);
             }
-        }
-        else {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
+          } else {
+            if (creep.memory.target === undefined) {
+              var sources = creep.room.find(FIND_SOURCES);
+              let source = sources[Math.floor(Math.random() * sources.length)];
+        
+              //var closest = creep.pos.findClosestByRange(FIND_SOURCES);
+              creep.memory.target = source.id;
+            }
+        
+            let source = Game.getObjectById(creep.memory.target);
+            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(source);
             }
         }
     }
