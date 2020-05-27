b80b585f892452b7e2ceb4c31f9f419addb31815
idesyatov <regull@bk.ru>
2020-05-28 00:01:57 +0300

    test

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ let debug = new Debug;
 /**@type {any}*/(Game).debug = debug;
 let mainOp = new MainOp;
 
-// test
 module.exports.loop = function() {
     /**@type {any}*/(Game).debug = debug;
     /**@type {any}*/(Game).main = mainOp;
